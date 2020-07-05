08e71b1f3dafcd99b072158f8e21ec62fd1721b2
Tyler Ewing <tewing10@gmail.com>
2020-07-05 18:54:53 -0500

    Add top-level with_dependency_next_override method

--- lib/double_take.rb
+++ lib/double_take.rb
@@ -1,6 +1,21 @@
+# frozen_string_literal: true
+
+require "bundler"
+require "pathname"
+
+require "double_take/clean"
 require "double_take/version"
 
 module DoubleTake
-  class Error < StandardError; end
-  # Your code goes here...
+  GEMFILE = Bundler.default_gemfile
+  GEMFILE_LOCK = Pathname("#{GEMFILE}.lock")
+  GEMFILE_NEXT_LOCK = Pathname("#{GEMFILE}_next.lock")
+
+  def with_dependency_next_override
+    ENV["DEPENDENCY_NEXT_OVERRIDE"] = "1"
+
+    yield
+  ensure
+    ENV.delete("DEPENDENCY_NEXT_OVERRIDE")
+  end
 end
--- spec/double_take_spec.rb
+++ spec/double_take_spec.rb
@@ -1,9 +1,29 @@
-RSpec.describe DoubleTake do
-  it "has a version number" do
-    expect(DoubleTake::VERSION).not_to be nil
-  end
+# frozen_string_literal: true
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe DoubleTake do
+  include described_class
+
+  describe "#with_dependency_next_override" do
+    it "yields to the block" do
+      expect do |block|
+        with_dependency_next_override(&block)
+      end.to yield_control
+    end
+
+    it "sets an env var" do
+      expect(ENV["DEPENDENCY_NEXT_OVERRIDE"]).not_to eq("1")
+      with_dependency_next_override do
+        expect(ENV["DEPENDENCY_NEXT_OVERRIDE"]).to eq("1")
+      end
+    end
+
+    it "ensures the environment variable is unset" do
+      expect do
+        with_dependency_next_override do
+          raise
+        end
+      end.to raise_error(RuntimeError)
+      expect(ENV["DEPENDENCY_NEXT_OVERRIDE"]).not_to eq("1")
+    end
   end
 end
